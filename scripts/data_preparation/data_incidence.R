e85430b636a47b92cd92c4bffa4218977d65d80b
SungmokJung <64189152+SungmokJung@users.noreply.github.com>
2022-10-07 20:47:57 +0200

    pop_country

--- scripts/data_preparation/data_incidence.R
+++ scripts/data_preparation/data_incidence.R
@@ -8,4 +8,7 @@ df_inci %>% arrange(date) %>% group_by(iso_code) %>% filter(rank(date)==1) %>%
   dplyr::select(iso_code, date) %>% rename(date_import = date) -> temp
 merge(df_inci, temp, by=c("iso_code"), all.x=TRUE) -> df_inci
 
+df_inci %>% dplyr::select(iso_code, location) %>% distinct() -> country_list
+
+write.csv(country_list, "../../data/incidence/country_list.csv")
 write.csv(df_inci, "../../data/incidence/df_inci.csv")
